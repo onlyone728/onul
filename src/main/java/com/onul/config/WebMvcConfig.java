90a1eaa8147ff7ed71a37e8a36fc69a61b4b248d
장하나 <onlyone728@gmail.com>
2022-03-23 19:40:54 +0900

    이미지 매핑 설정

---
+++ src/main/java/com/onul/config/WebMvcConfig.java
@@ -0,0 +1,18 @@
+package com.onul.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfig implements WebMvcConfigurer {
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry
+		.addResourceHandler("/images/**")
+		.addResourceLocations("file:/Users/janghana/Desktop/java/megait/7_portfolio/onul/workspace/images/");
+	}
+	
+	
+}
