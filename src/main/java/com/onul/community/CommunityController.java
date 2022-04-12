99109743357622738f400dda64e4a0486e228c05
장하나 <onlyone728@gmail.com>
2022-04-12 17:22:26 +0900

    쿠키 추가, hit 기능 추가

--- src/main/java/com/onul/community/CommunityController.java
+++ src/main/java/com/onul/community/CommunityController.java
@@ -2,7 +2,9 @@ package com.onul.community;
 
 import java.util.List;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,7 +19,10 @@ import com.onul.community.bo.PhotoViewBO;
 import com.onul.community.model.IntroduceHouseView;
 import com.onul.community.model.KnowhowView;
 import com.onul.community.model.PhotoView;
+import com.onul.introduceHousePost.bo.IntroduceHouseBO;
+import com.onul.knowhowPost.bo.KnowhowBO;
 import com.onul.knowhowPost.model.Category;
+import com.onul.photo.bo.PhotoBO;
 import com.onul.photo.model.Space;
 import com.onul.shop.bo.ProductViewBO;
 import com.onul.shop.model.ProductView;
@@ -26,10 +31,19 @@ import com.onul.shop.model.ProductView;
 public class CommunityController {
 	
 	@Autowired
+	private PhotoBO photoBO;
+	
+	@Autowired
 	private PhotoViewBO photoViewBO;
 	
 	@Autowired
+	private IntroduceHouseBO introBO;
+	
+	@Autowired
 	private IntroduceHouseViewBO introduceBO;
+	
+	@Autowired
+	private KnowhowBO howBO;
 	
 	@Autowired
 	private KnowhowViewBO knowhowBO;
@@ -65,66 +79,144 @@ public class CommunityController {
 	public String photoDetailView(
 			@RequestParam("postId") int postId,
 			Model model,
-			HttpServletRequest request) {
+			HttpServletRequest request,
+			HttpServletResponse response) {
 		
 		// session 가져오기
 		HttpSession session = request.getSession();
 		Integer userId = (Integer) session.getAttribute("userId");
 		
-		// hit 증가
+		// hit 증가(cookie)
+		Cookie[] cookies = request.getCookies();
+		Cookie viewCookie = null;	// 비교하기위한 새로운 쿠키
 		
+		 // 쿠키가 있을 경우 
+        if (cookies != null && cookies.length > 0) {
+        	for (int i = 0; i < cookies.length; i++)
+            {
+                // Cookie의 name이 cookie + postId와 일치하는 쿠키를 viewCookie에 넣어줌 
+                if (cookies[i].getName().equals("cookiePhoto" + postId)) { 
+                    viewCookie = cookies[i];
+                }
+            }
+        }
 		
 		// BO -> DB select : photo & userInfo
 		PhotoView photoView = photoViewBO.photoViewByPostId(postId, userId);
-		
-		model.addAttribute("photoView", photoView);
-		model.addAttribute("viewPath", "community/detail_photo");
-		
-		return "template/layout";
+		if (photoView != null) {
+            if (viewCookie == null) {    // 만일 viewCookie가 null일 경우 쿠키를 생성해서 조회수 증가 로직을 처리함.
+                // 쿠키 생성(이름, 값)
+                Cookie newCookie = new Cookie("cookiePhoto"+postId, "|" + postId + "|");
+                                
+                // 쿠키 추가
+                response.addCookie(newCookie);
+ 
+                // 쿠키를 추가 시키고 조회수 증가시킴
+                photoBO.addHit(postId);
+            } 
+            photoView = photoViewBO.photoViewByPostId(postId, userId);
+            model.addAttribute("photoView", photoView);
+            model.addAttribute("viewPath", "community/detail_photo");
+            return "template/layout";
+		} else {
+			return "redirect:/community/photo_view";
+		}
 	}
 	
 	@RequestMapping("/community/introduce_detail_view")
 	public String detailIntroduce(
 			@RequestParam("postId") int postId,
 			Model model,
-			HttpServletRequest request) {
+			HttpServletRequest request,
+			HttpServletResponse response) {
 		
 		// session 가져오기
 		HttpSession session = request.getSession();
 		Integer userId = (Integer) session.getAttribute("userId");
 		
-		// hit 증가
-//		introduceBO.addHit(postId);
+		// hit 증가(cookie)
+		Cookie[] cookies = request.getCookies();
+		Cookie viewCookie = null;	// 비교하기위한 새로운 쿠키
+		
+		 // 쿠키가 있을 경우 
+        if (cookies != null && cookies.length > 0) {
+        	for (int i = 0; i < cookies.length; i++)
+            {
+                // Cookie의 name이 cookie + postId와 일치하는 쿠키를 viewCookie에 넣어줌 
+                if (cookies[i].getName().equals("cookieHouse" + postId)) { 
+                    viewCookie = cookies[i];
+                }
+            }
+        }
 		
 		// DB select 
 		IntroduceHouseView house = introduceBO.generateIntroduceHouseView(postId, userId);
-		
-		model.addAttribute("post", house);
-		model.addAttribute("viewPath", "community/detail_introduce");
-		
-		return "template/layout";
+		if (house != null) {
+            if (viewCookie == null) {    // 만일 viewCookie가 null일 경우 쿠키를 생성해서 조회수 증가 로직을 처리함.
+                // 쿠키 생성(이름, 값)
+                Cookie newCookie = new Cookie("cookieHouse"+postId, "|" + postId + "|");
+                                
+                // 쿠키 추가
+                response.addCookie(newCookie);
+ 
+                // 쿠키를 추가 시키고 조회수 증가시킴
+                introBO.addHit(postId);
+            } 
+            house = introduceBO.generateIntroduceHouseView(postId, userId);
+            model.addAttribute("post", house);
+    		model.addAttribute("viewPath", "community/detail_introduce");
+            return "template/layout";
+		} else {
+			return "redirect:/community/introduce_view";
+		}
 	}
 	
 	@RequestMapping("/community/knowhow_detail_view")
 	public String detailKnowhow(
 			@RequestParam("postId") int postId,
 			Model model,
-			HttpServletRequest request) {
+			HttpServletRequest request,
+			HttpServletResponse response) {
 		
 		// session 가져오기
 		HttpSession session = request.getSession();
 		Integer userId = (Integer) session.getAttribute("userId");
 		
+		// hit 증가(cookie)
+		Cookie[] cookies = request.getCookies();
+		Cookie viewCookie = null;	// 비교하기위한 새로운 쿠키
 		
-		// hit 증가
-//		introduceBO.addHit(postId);
+		 // 쿠키가 있을 경우 
+        if (cookies != null && cookies.length > 0) {
+        	for (int i = 0; i < cookies.length; i++)
+            {
+                // Cookie의 name이 cookie + postId와 일치하는 쿠키를 viewCookie에 넣어줌 
+                if (cookies[i].getName().equals("cookieKnowhow" + postId)) { 
+                    viewCookie = cookies[i];
+                }
+            }
+        }
 		
 		// DB select
 		KnowhowView knowhow = knowhowBO.generateKnowhowView(postId, userId);
-		
-		model.addAttribute("post", knowhow);
-		model.addAttribute("viewPath", "community/detail_knowhow");
-		return "template/layout";
+		if (knowhow != null) {
+            if (viewCookie == null) {    // 만일 viewCookie가 null일 경우 쿠키를 생성해서 조회수 증가 로직을 처리함.
+                // 쿠키 생성(이름, 값)
+                Cookie newCookie = new Cookie("cookieKnowhow"+postId, "|" + postId + "|");
+                                
+                // 쿠키 추가
+                response.addCookie(newCookie);
+ 
+                // 쿠키를 추가 시키고 조회수 증가시킴
+                howBO.addHit(postId);
+            } 
+            knowhow = knowhowBO.generateKnowhowView(postId, userId);
+            model.addAttribute("post", knowhow);
+    		model.addAttribute("viewPath", "community/detail_knowhow");
+    		return "template/layout";
+		} else {
+			return "redirect:/community/knowhow_view";
+		}
 	}
 	
 	@RequestMapping("/community/photo_view")
